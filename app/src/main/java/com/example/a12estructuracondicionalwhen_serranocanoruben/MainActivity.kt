773a9c945919c410cd8e7cb5d23f5fb813a49cc8
Cherry708 <aluser8241@ieselcaminas.org>
2021-09-20 19:20:51 +0200

    1

---
+++ app/src/main/java/com/example/a12estructuracondicionalwhen_serranocanoruben/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a12estructuracondicionalwhen_serranocanoruben
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
