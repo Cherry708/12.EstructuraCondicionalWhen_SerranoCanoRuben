f199265b9b5f2d05ff1a4237b3f17aeac7840b81
Cherry708 <aluser8241@ieselcaminas.org>
2021-09-21 11:17:17 +0200

    Comentarios de errores añadidos

--- app/src/main/java/com/example/a12estructuracondicionalwhen_serranocanoruben/MainActivity.kt
+++ app/src/main/java/com/example/a12estructuracondicionalwhen_serranocanoruben/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.a12estructuracondicionalwhen_serranocanoruben
 
+import android.net.wifi.p2p.WifiP2pManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etXPoint = findViewById<EditText>(R.id.etXPoint)
+        val etYPoint = findViewById<EditText>(R.id.etYPoint)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        //DEBUGGER: Al empeza está vacío
+        val xPoint = etXPoint.text.toString()
+        val yPoint = etYPoint.text.toString()
+        //Aún escribiendo siempre está empty, núnca recoge nada, excepto una vez. Inconsistente
+        btnCalculate.setOnClickListener{
+            if (xPoint.isNotEmpty() && yPoint.isNotEmpty()){
+                val xPoint = etXPoint.text.toString().toInt()
+                val yPoint = etYPoint.text.toString().toInt()
+                Toast.makeText(this, calculateQuadrant(xPoint, yPoint), Toast.LENGTH_LONG).show()
+
+            }
+        }
+
     }
+    private fun calculateQuadrant(xPoint:Int, yPoint:Int): String{
+        var result = "Point is on axis"
+        when{
+            xPoint > 0 && yPoint > 0 -> result = "First quadrant."
+            xPoint < 0 && yPoint > 0 -> result = "Second quadrant."
+            xPoint < 0 && yPoint < 0 -> result = "Third quadrant"
+            xPoint > 0 && yPoint < 0 -> result = "Fourth quadrant"
+        }
+        return result
+    }
+
 }
 
---
+++ app/src/main/java/com/example/a12estructuracondicionalwhen_serranocanoruben/Problema2.kt
@@ -0,0 +1,41 @@
+package com.example.a12estructuracondicionalwhen_serranocanoruben
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class Problema2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_problema2)
+
+        val etFirstGrade = findViewById<EditText>(R.id.etFirstGrade)
+        val etSecondGrade = findViewById<EditText>(R.id.etSecondGrade)
+        val etThirdGrade = findViewById<EditText>(R.id.etThirdGrade)
+        val btnCalculateAverage = findViewById<Button>(R.id.btnCalculateAverage)
+
+        val firstGrade = etFirstGrade.text.toString()
+        val secondGrade = etSecondGrade.text.toString()
+        val thirdGrade = etThirdGrade.text.toString()
+
+
+        var average = 0.0
+        var result = ""
+        btnCalculateAverage.setOnClickListener {
+            if (firstGrade.isNotEmpty() && secondGrade.isNotEmpty() && thirdGrade.isNotEmpty()){
+                val firstGradeDouble = etFirstGrade.text.toString().toDouble()
+                val secondGradeDouble = etSecondGrade.text.toString().toDouble()
+                val thirdGradeDouble = etThirdGrade.text.toString().toDouble()
+                average = (firstGradeDouble+secondGradeDouble+thirdGradeDouble)/3
+            }
+            when{
+                average >= 7 -> result = "Promocionado."
+                average >= 4 -> result = "Regular."
+                else -> result = "Libre."
+            }
+            Toast.makeText(this,result,Toast.LENGTH_LONG).show()
+        }
+    }
+}
